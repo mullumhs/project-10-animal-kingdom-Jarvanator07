c60e4844723adad11a57a6824aa2befcf918d487
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-05-22 10:40:00 +1000

    sgsds

--- lab1.py
+++ lab1.py
@@ -12,25 +12,41 @@
 # This class should have an initialiser with at least three attributes. E.g. name, age, and habitat.
 # Add at least two methods for common animal behaviors. E.g. eat and sleep.
 
+class Animal:
+    def __init__(self, name: str, age: int, habitat: str) -> None:
+        self.name = name
+        self.age = age
+        self.habitat = habitat
 
+    def eat(self):
+        print(f"{self.name} is eating.")
 
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
 
 
 # Create at least two derived classes from the Animal class. E.g. Bird and Fish.
 # Give each of the derived classes at least one specific behavior. E.g. fly and swim.
+            
+class Bird(Animal):
+    def fly(self):
+        print(f"{self.name} is flying.")
 
-
-
-
+class Fish(Animal):
+    def swim(self):
+        print(f"{self.name} is swimming.")
 
 # Create at least two instances of the Animal derived classes with different data.
 
-
-
-
+eagle = Bird("Eagle", 10, "forest")
+shark = Fish("Shark", 15, "Ocean")
 
 # Write code that prints out the details of each animal and calls their specific behaviors.
 
-
-
+print(eagle.name, eagle.age, eagle.habitat)
+print(shark.name, shark.age, shark.habitat)
+print(eagle.fly())
+print(shark.swim())
+print(eagle.eat())
+print(shark.eat())
 
--- lab2.py
+++ lab2.py
@@ -10,15 +10,29 @@
 
 # Paste your base class Animal and the derived classes from Lab 1 here.
 
+class Animal:
+    def __init__(self, name: str, age: int, habitat: str) -> None:
+        self.name = name
+        self.age = age
+        self.habitat = habitat
 
+    def eat(self):
+        print(f"{self.name} is eating.")
 
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
 
+class Bird(Animal):
+    def fly(self):
+        print(f"{self.name} is flying.")
+
+class Fish(Animal):
+    def swim(self):
+        print(f"{self.name} is swimming.")
 
 # Modify the classes to demonstrate polymorphism through method overriding.
 # Each derived class should override at least one method from the Animal class.
 # For instance, you might want to override a 'make_sound' method to reflect the specific sound each animal makes.
-
-
 
 
 
